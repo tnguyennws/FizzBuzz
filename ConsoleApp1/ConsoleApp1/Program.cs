3b2b2603d975f125b4b814cb50a7b3a3ee90d651
Timothée <tnguyen@normandiewebschool.fr>
2021-09-21 11:50:07 +0200

    First commit
    FizzBuzz

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.Write($"{i} ");
+
+                if(i%3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if(i%5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
